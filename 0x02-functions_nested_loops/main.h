bb5f6d81ddd5aa026b7def02aaafe7af82c23d3d
Vuritch <omarabderhman123@gmail.com>
2023-09-13 17:35:34 -0700

    asdasd

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
  * File: holberton.h
@@ -7,7 +7,7 @@
  * Desc: Header file containing declarations for all functions
  *       used in the 0x02-functions_nested_loops directory.
  */
-
+#include <studio.h>
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);

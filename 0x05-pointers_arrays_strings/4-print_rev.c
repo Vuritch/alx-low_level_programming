18dcf0f8fccfd57d85009d3bcdb48567899b6030
Vuritch <omarabderhman123@gmail.com>
2023-09-18 17:18:31 -0700

    azxczxC

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,14 +6,15 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		len++;
+	while (s[i])
+		i++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar('\n');
 }
 

d06b4a95657c48bd3c736ebecc0da21fb205a8b6
Vuritch <omarabderhman123@gmail.com>
2023-09-21 17:58:41 -0700

    fzxcx

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,14 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
-
-int _putchar(char c);
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
